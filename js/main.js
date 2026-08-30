ccff977bfdd9af423be63584fe86d33b0c4a9c68
rickyusu <rickyusu@gmail.com>
2026-08-30 07:48:37 -0700

    2

--- js/main.js
+++ js/main.js
@@ -151,15 +151,15 @@ const commonFooterCode = `
       
     <p style="text-align: center;">  
     <a href="https://www.neijin-qigong.com/" aria-label="German" style="text-decoration: none;">
-      <img src="images/yzc-logo.png" alt=" " style="width: 50px; height: auto; ">
+      <img src="images/yzc-logo.png" alt=" " style="width: 45px; height: auto; ">
     </a>
     &ensp;
     <a href="about.html" aria-label="YZC" >
-      <img src="images/yzc_cnlogo.png" alt=" " style="width: 200px; height: auto;"class="center-image">
+      <img src="images/yzc_cnlogo.png" alt=" " style="width: 180px; height: auto;"class="center-image">
     </a>
     &ensp;
     <a href="index2.html" aria-label="Chinese">
-      <img src="images/yzc-stamp.gif" alt=" " style="width: auto; height:45px;">
+      <img src="images/yzc-stamp.gif" alt=" " style="width: auto; height:40px;">
     </a>
 
     </p>
