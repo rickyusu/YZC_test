249f131a2230925161c694c1db3a5ab950b88856
rickyusu <rickyusu@gmail.com>
2026-09-12 17:42:50 -0700

    1

--- js/main.js
+++ js/main.js
@@ -71,7 +71,6 @@ if (hero && banners.length > 0) {
             image.onload = resolve;
             image.onerror = resolve;
             image.src = src;
-            image.style.imageRendering = "-webkit-optimize-contrast"; /* added */
         });
     });
 
